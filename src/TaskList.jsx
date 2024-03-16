2c95ddb0477ac72440dc2cdf8f09a67936421c91
Pri Sci <rpandur@ucol.m,x>
2024-03-15 21:38:55 -0600

    Agregados cambios en TaskList.jsx e index.css

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,10 +1,21 @@
-
 import React from 'react';
-import Task from './Task';
+import Task from './Task'; // Importa el componente Task
+import './index.css'; // Importa el archivo de estilos CSS
 
+// Componente TaskList que recibe las tareas y la función de cambio de estado como props
 const TaskList = ({ tasks, handleTaskToggle }) => {
+  // Contar las tareas completadas
+  const completedTasksCount = tasks.filter(task => task.completed).length;
+  // Contar las tareas no completadas
+  const pendingTasksCount = tasks.length - completedTasksCount;
+
   return (
     <div>
+      {/* Muestra el título y la cantidad de tareas completadas */}
+      <h2 id='hechas'>Tareas Realizadas: {completedTasksCount}</h2>
+      {/* Muestra el título y la cantidad de tareas pendientes */}
+      <h2 id='nohechas'>Tareas Pendientes: {pendingTasksCount}</h2>
+      {/* Mapea sobre cada tarea y renderiza el componente Task, pasando las props necesarias */}
       {tasks.map(task => (
         <Task key={task.id} task={task} handleTaskToggle={handleTaskToggle} />
       ))}
